6ec5b917bd934944ff28da17e3f7b5224e6e5a2d
Tu Nguyen <ladiesman9x@gmail.com>
2020-02-28 08:00:03 +0700

    Update config

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,12 @@ import '../node_modules/firebase/storage';
 
 // Initalize and export Firebase.
 const config = {
-    apiKey: 'xxxxxx',
-    authDomain: 'xxxxx.firebaseapp.com',
-    databaseURL: 'https://xxxx.firebaseio.com',
+    apiKey: 'AIzaSyD2QEKNa5MPDYCO2Ui6r9EJRNf9E_33xDY',
+    authDomain: 'mrtuvn-music-collection.firebaseapp.com',
+    databaseURL: 'https://mrtuvn-music-collection.firebaseio.com',
     projectId: 'mrtuvn-music-collection',
-    storageBucket: '',
-    messagingSenderId: 'xxxxx'
+    storageBucket: 'mrtuvn-music-collection.appspot.com',
+    messagingSenderId: '742619806435',
+    appId: '1:742619806435:web:12e439c56ccf14164de3d6'
 };
 export default firebase.initializeApp(config);
